7c9509337cfc02764f97dde07d093b8a43971c77
Anders Pitman <tapitman11@gmail.com>
2017-03-04 07:46:30 -0700

    Submission

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var min = 9999;
-var max = 0;
+var mins = {};
+var maxes = {};
+
 const year = 2015;
 const divisionsCount = 7;
 
@@ -17,6 +18,22 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token='
     //attribution: ...
 }).addTo(mapQuantile);
 
+var smallMaps = [];
+for (var i = 0; i < 6; i++) {
+  const currYear = 2010 + i;
+  const mapId = "map_small_multiple_" + currYear;
+  //const elem = document.getElementById(mapId);
+
+  var smallMap = L.map(mapId, { zoomControl: false }).setView([34.0489, -111.0937], 5);
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+      id: 'mapbox.light'
+      //attribution: ...
+  }).addTo(smallMap);
+
+  smallMaps.push(smallMap);
+}
+
 var parseOptions = {
   header: true,
   download: true,
@@ -32,6 +49,13 @@ function dataReady(results) {
 
   L.geoJson(countyData, { style: styleEqualInterval }).addTo(map);
   L.geoJson(countyData, { style: styleQuantile }).addTo(mapQuantile);
+
+  smallMaps.forEach(function(smallMap, index) {
+    var styleFunc = function(feature) {
+      return styleSmallMultiple(feature, 2010 + index);
+    };
+    L.geoJson(countyData, { style: styleFunc }).addTo(smallMap);
+  });
 }
 
 function convertData(data) {
@@ -44,24 +68,35 @@ function convertData(data) {
 
 function populateGeoJson(data, geojson) {
   geojson.features.forEach(function(county) {
+
+    county.properties.dissolutionData = {};
+
     data.forEach(function(row) {
-      if (row.year === year && row.county === county.properties.NAME) {
-        county.properties.dissolutionRate = row.dissolutionRate;
-        if (row.dissolutionRate < min) {
-          min = row.dissolutionRate;
-        }
-        if (row.dissolutionRate > max) {
-          max = row.dissolutionRate;
-        }
+
+      if (row.county === county.properties.NAME) {
+        county.properties.dissolutionData[row.year] = row.dissolutionRate;
       }
+
+      if (!mins[row.year]) mins[row.year] = 9999;
+      if (!maxes[row.year]) maxes[row.year] = 0;
+
+      if(row.dissolutionRate < mins[row.year]) {
+        mins[row.year] = row.dissolutionRate;
+      }
+      if(row.dissolutionRate > maxes[row.year]) {
+        maxes[row.year] = row.dissolutionRate;
+      }
+
     });
   });
+
+  console.log(geojson);
 }
 
 const colors = ['#feedde','#fdd0a2','#fdae6b','#fd8d3c','#f16913','#d94801','#8c2d04'];
 
-function colorEqualInterval(d) {
-  var divisionSize = (max - min) / (divisionsCount - 1);
+function colorEqualInterval(d, year) {
+  var divisionSize = (maxes[year] - mins[year]) / (divisionsCount - 1);
   var index = Math.floor(d / divisionSize) - 1;
   return colors[index];
 }
@@ -88,9 +123,13 @@ function style(color) {
 }
 
 function styleEqualInterval(feature) {
-  return style(colorEqualInterval(feature.properties.dissolutionRate));
+  return style(colorEqualInterval(feature.properties.dissolutionData[year], year));
 }
 
 function styleQuantile(feature) {
-  return style(colorQuantiles(feature.properties.dissolutionRate));
+  return style(colorQuantiles(feature.properties.dissolutionData[year]));
+}
+
+function styleSmallMultiple(feature, year) {
+  return style(colorEqualInterval(feature.properties.dissolutionData[year], year));
 }
